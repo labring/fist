0eeed8b733fe18a898d7ab9367f8c38d021f150f
fanux <fhtjob@hotmail.com>
2019-02-20 15:49:05 +0800

    add

--- auth/main.go
+++ auth/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
 	"crypto/rand"
 	"crypto/rsa"
 	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/emicklei/go-restful"
 	jose "gopkg.in/square/go-jose.v2"
@@ -114,7 +120,98 @@ func discoveryHandler(request *restful.Request, response *restful.Response) {
 	response.WriteEntity(dis)
 }
 
+// Determine the signature algorithm for a JWT.
+func signatureAlgorithm(jwk *jose.JSONWebKey) (alg jose.SignatureAlgorithm, err error) {
+	if jwk.Key == nil {
+		return alg, errors.New("no signing key")
+	}
+	switch key := jwk.Key.(type) {
+	case *rsa.PrivateKey:
+		// Because OIDC mandates that we support RS256, we always return that
+		// value. In the future, we might want to make this configurable on a
+		// per client basis. For example allowing PS256 or ECDSA variants.
+		//
+		// See https://github.com/coreos/dex/issues/692
+		return jose.RS256, nil
+	case *ecdsa.PrivateKey:
+		// We don't actually support ECDSA keys yet, but they're tested for
+		// in case we want to in the future.
+		//
+		// These values are prescribed depending on the ECDSA key type. We
+		// can't return different values.
+		switch key.Params() {
+		case elliptic.P256().Params():
+			return jose.ES256, nil
+		case elliptic.P384().Params():
+			return jose.ES384, nil
+		case elliptic.P521().Params():
+			return jose.ES512, nil
+		default:
+			return alg, errors.New("unsupported ecdsa curve")
+		}
+	default:
+		return alg, fmt.Errorf("unsupported signing key type %T", key)
+	}
+}
+
+type idTokenClaims struct {
+	Issuer           string `json:"iss"`
+	Subject          string `json:"sub"`
+	Audience         string `json:"aud"`
+	Expiry           int64  `json:"exp"`
+	IssuedAt         int64  `json:"iat"`
+	AuthorizingParty string `json:"azp,omitempty"`
+	Nonce            string `json:"nonce,omitempty"`
+
+	AccessTokenHash string `json:"at_hash,omitempty"`
+
+	Email         string `json:"email,omitempty"`
+	EmailVerified *bool  `json:"email_verified,omitempty"`
+
+	Groups []string `json:"groups,omitempty"`
+
+	Name string `json:"name,omitempty"`
+
+	FederatedIDClaims *federatedIDClaims `json:"federated_claims,omitempty"`
+}
+
+type federatedIDClaims struct {
+	ConnectorID string `json:"connector_id,omitempty"`
+	UserID      string `json:"user_id,omitempty"`
+}
+
 func handlerToken(request *restful.Request, response *restful.Response) {
+	signingAlg, err := signatureAlgorithm(Priv)
+	if err != nil {
+		return
+	}
+
+	ev := true
+
+	tok := idTokenClaims{
+		Issuer:        "https://dex.example.com:8080",
+		Subject:       "Cgc4OTEyNTU3EgZnaXRodWI",
+		Audience:      "example-app",
+		Expiry:        time.Now().Add(time.Minute * 10).Unix(),
+		IssuedAt:      time.Now().Unix(),
+		Email:         "fhtjob@hotmail.com",
+		EmailVerified: &ev,
+		Groups:        []string{"dev"},
+		Name:          "fanux",
+	}
+
+	payload, err := json.Marshal(tok)
+	if err != nil {
+		fmt.Println("could not serialize claims", err)
+		return
+	}
+
+	if idToken, err = signPayload(signingKey, signingAlg, payload); err != nil {
+		fmt.Println("failed to sign payload", err)
+		return
+	}
+
+	response.WriteEntity(idToken)
 }
 
 func newUUID() string {
