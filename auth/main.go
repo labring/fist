5675ac0808147102ee5449e68fc0f76578347042
fanux <fhtjob@hotmail.com>
2019-02-20 10:12:38 +0800

    add

--- auth/main.go
+++ auth/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/emicklei/go-restful"
+	jose "github.com/square/go-jose"
 )
 
 // This example has the same service definition as restful-user-resource
@@ -46,6 +47,63 @@ func (u UserResource) Register(container *restful.Container) {
 	ws.Route(ws.DELETE("/{user-id}").To(u.removeUser))
 
 	container.Add(ws)
+
+	auth := new(restful.WebService)
+	auth.
+		Path("/").
+		Consumes(restful.MIME_XML, restful.MIME_JSON).
+		Produces(restful.MIME_JSON, restful.MIME_XML) // you can specify this per route as well
+
+	auth.Route(auth.GET("/.well-known/openid-configuration").To(discoveryHandler))
+	auth.Route(auth.GET("/token").To(handlerToken))
+	auth.Route(auth.GET("/keys").To(handlePublicKeys))
+
+	container.Add(auth)
+
+}
+
+func discoveryHandler(request *restful.Request, response *restful.Response) {
+	type discovery struct {
+		Issuer        string   `json:"issuer"`
+		Auth          string   `json:"authorization_endpoint"`
+		Token         string   `json:"token_endpoint"`
+		Keys          string   `json:"jwks_uri"`
+		ResponseTypes []string `json:"response_types_supported"`
+		Subjects      []string `json:"subject_types_supported"`
+		IDTokenAlgs   []string `json:"id_token_signing_alg_values_supported"`
+		Scopes        []string `json:"scopes_supported"`
+		AuthMethods   []string `json:"token_endpoint_auth_methods_supported"`
+		Claims        []string `json:"claims_supported"`
+	}
+
+	dis := &discovery{
+		Issuer:      "https://dex.example.com:8080",
+		Auth:        "https://dex.example.com:8080/auth",
+		Token:       "https://dex.example.com:8080/token",
+		Keys:        "https://dex.example.com:8080/keys",
+		Subjects:    []string{"public"},
+		IDTokenAlgs: []string{string(jose.RS256)},
+		Scopes:      []string{"openid", "email", "groups", "profile", "offline_access"},
+		AuthMethods: []string{"client_secret_basic"},
+		Claims: []string{
+			"aud", "email", "email_verified", "exp",
+			"iat", "iss", "locale", "name", "sub",
+		},
+		ResponseTypes: []string{"code",
+			"token",
+			"id_token",
+			"code token",
+			"code id_token",
+			"token id_token",
+			"code token id_token",
+			"none"},
+	}
+
+	response.WriteEntity(dis)
+}
+func handlerToken(request *restful.Request, response *restful.Response) {
+}
+func handlePublicKeys(request *restful.Request, response *restful.Response) {
 }
 
 // GET http://localhost:8080/users/1
