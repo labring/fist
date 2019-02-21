faa55367efc1eeea0424950911fd3c0ccd0f0c3a
fanux <fhtjob@hotmail.com>
2019-02-21 20:00:40 +0800

    add

---
+++ auth/ssl/k8s-test.sh
@@ -0,0 +1,6 @@
+curl -H "Authorization: Bearer $1" \
+ -k https://172.31.12.61:6443/api/v1/namespaces/default/pods
+
+
+echo $1
+
---
+++ auth/ssl/well.sh
@@ -0,0 +1,2 @@
+curl https://dex.example.com:8080/.well-known/openid-configuration --cacert ca.pem
+curl -v https://dex.example.com:8080/keys --cacert ca.pem
