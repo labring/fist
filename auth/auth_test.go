86979951f3aa482efe00aa18aec5ac2b55a7e696
fanux <fhtjob@hotmail.com>
2019-02-22 13:51:10 +0800

    add docker file

---
+++ auth/auth_test.go
@@ -0,0 +1,42 @@
+package auth
+
+/*
+	fmt.Println("=========veriry========\n\n")
+	object, err := jose.ParseSigned(idToken)
+	if err != nil {
+		fmt.Printf("parse signed failed: %s", err)
+	}
+
+	// Now we can verify the signature on the payload. An error here would
+	// indicate the the message failed to verify, e.g. because the signature was
+	// broken or the message was tampered with.
+	priv, ok := Priv.Key.(*rsa.PrivateKey)
+	if !ok {
+		fmt.Println("to rsa private key failed")
+	}
+	output, err := object.Verify(priv.Public())
+	if err != nil {
+		fmt.Printf("Verify failed: %s", err)
+	}
+
+	fmt.Printf(string(output))
+	fmt.Println("=========veriry========\n\n")
+
+	fmt.Println("=========oidc verify========\n\n")
+	ctx := context.Background()
+	provider, err := oidc.NewProvider(ctx, "https://dex.example.com:8080")
+	if err != nil {
+		fmt.Println("new provider failed: ", err)
+	}
+	oidcConfig := &oidc.Config{
+		ClientID: "example-app",
+	}
+	verifier := provider.Verifier(oidcConfig)
+	token, err := verifier.Verify(ctx, idToken)
+	if err != nil {
+		fmt.Println("valify failed: ", err)
+	}
+	fmt.Println("token: ", token)
+	fmt.Println("=========oidc verify========\n\n")
+
+*/
--- auth/main.go
+++ auth/main.go
@@ -1,72 +1,24 @@
 package main
 
 import (
-	"context"
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
-	"crypto/rsa"
-	"encoding/hex"
 	"encoding/json"
-	"errors"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"time"
 
-	oidc "github.com/coreos/go-oidc"
 	"github.com/emicklei/go-restful"
 	jose "gopkg.in/square/go-jose.v2"
 )
-
-// This example has the same service definition as restful-user-resource
-// but uses a different router (CurlyRouter) that does not use regular expressions
-//
-// POST http://localhost:8080/users
-// <User><ID>1</ID><Name>Melissa Raspberry</Name></User>
-//
-// GET http://localhost:8080/users/1
-//
-// PUT http://localhost:8080/users/1
-// <User><ID>1</ID><Name>Melissa</Name></User>
-//
-// DELETE http://localhost:8080/users/1
-//
-
-//User is
-type User struct {
-	ID, Name string
-}
 
 //key paires
 var (
 	Pub  jose.JSONWebKey
 	Priv jose.JSONWebKey
-	Ver  jose.JSONWebKey
 )
-
-//UserResource is
-type UserResource struct {
-	// normally one would use DAO (data access object)
-	users map[string]User
-}
 
 //Register is
 func (u UserResource) Register(container *restful.Container) {
-	ws := new(restful.WebService)
-	ws.
-		Path("/users").
-		Consumes(restful.MIME_XML, restful.MIME_JSON).
-		Produces(restful.MIME_JSON, restful.MIME_XML) // you can specify this per route as well
-
-	ws.Route(ws.GET("/{user-id}").To(u.findUser))
-	ws.Route(ws.POST("").To(u.updateUser))
-	ws.Route(ws.PUT("/{user-id}").To(u.createUser))
-	ws.Route(ws.DELETE("/{user-id}").To(u.removeUser))
-
-	container.Add(ws)
-
 	Pub, Priv = CreateKeyPair()
 
 	auth := new(restful.WebService)
@@ -125,81 +77,10 @@ func discoveryHandler(request *restful.Request, response *restful.Response) {
 	response.WriteEntity(dis)
 }
 
-// Determine the signature algorithm for a JWT.
-func signatureAlgorithm(jwk *jose.JSONWebKey) (alg jose.SignatureAlgorithm, err error) {
-	if jwk.Key == nil {
-		return alg, errors.New("no signing key")
-	}
-	switch key := jwk.Key.(type) {
-	case *rsa.PrivateKey:
-		// Because OIDC mandates that we support RS256, we always return that
-		// value. In the future, we might want to make this configurable on a
-		// per client basis. For example allowing PS256 or ECDSA variants.
-		//
-		// See https://github.com/coreos/dex/issues/692
-		return jose.RS256, nil
-	case *ecdsa.PrivateKey:
-		// We don't actually support ECDSA keys yet, but they're tested for
-		// in case we want to in the future.
-		//
-		// These values are prescribed depending on the ECDSA key type. We
-		// can't return different values.
-		switch key.Params() {
-		case elliptic.P256().Params():
-			return jose.ES256, nil
-		case elliptic.P384().Params():
-			return jose.ES384, nil
-		case elliptic.P521().Params():
-			return jose.ES512, nil
-		default:
-			return alg, errors.New("unsupported ecdsa curve")
-		}
-	default:
-		return alg, fmt.Errorf("unsupported signing key type %T", key)
-	}
-}
-
-type idTokenClaims struct {
-	Issuer           string `json:"iss"`
-	Subject          string `json:"sub"`
-	Audience         string `json:"aud"`
-	Expiry           int64  `json:"exp"`
-	IssuedAt         int64  `json:"iat"`
-	AuthorizingParty string `json:"azp,omitempty"`
-	Nonce            string `json:"nonce,omitempty"`
-
-	AccessTokenHash string `json:"at_hash,omitempty"`
-
-	Email         string `json:"email,omitempty"`
-	EmailVerified *bool  `json:"email_verified,omitempty"`
-
-	Groups []string `json:"groups,omitempty"`
-
-	Name string `json:"name,omitempty"`
-
-	FederatedIDClaims *federatedIDClaims `json:"federated_claims,omitempty"`
-}
-
-type federatedIDClaims struct {
-	ConnectorID string `json:"connector_id,omitempty"`
-	UserID      string `json:"user_id,omitempty"`
-}
-
-func signPayload(key *jose.JSONWebKey, alg jose.SignatureAlgorithm, payload []byte) (jws string, err error) {
-	signingKey := jose.SigningKey{Key: key, Algorithm: alg}
-
-	signer, err := jose.NewSigner(signingKey, &jose.SignerOptions{})
-	if err != nil {
-		return "", fmt.Errorf("new signier: %v", err)
-	}
-	signature, err := signer.Sign(payload)
-	if err != nil {
-		return "", fmt.Errorf("signing payload: %v", err)
-	}
-	return signature.CompactSerialize()
-}
-
 func handlerToken(request *restful.Request, response *restful.Response) {
+	groups := request.QueryParameters("group")
+	user := request.QueryParameters("user")
+
 	signingAlg, err := signatureAlgorithm(&Priv)
 	if err != nil {
 		fmt.Println("failed to sign payload", err)
@@ -207,7 +88,6 @@ func handlerToken(request *restful.Request, response *restful.Response) {
 	}
 
 	ev := true
-
 	tok := idTokenClaims{
 		Issuer:        "https://dex.example.com:8080",
 		Subject:       "Cgc4OTEyNTU3EgZnaXRodWI",
@@ -216,8 +96,8 @@ func handlerToken(request *restful.Request, response *restful.Response) {
 		IssuedAt:      time.Now().Unix(),
 		Email:         "fhtjob@hotmail.com",
 		EmailVerified: &ev,
-		Groups:        []string{"dev"},
-		Name:          "fanux",
+		Groups:        groups,
+		Name:          user,
 	}
 
 	payload, err := json.Marshal(&tok)
@@ -234,87 +114,7 @@ func handlerToken(request *restful.Request, response *restful.Response) {
 	}
 
 	fmt.Println("token: ", idToken)
-
-	fmt.Println("=========veriry========\n\n")
-	object, err := jose.ParseSigned(idToken)
-	if err != nil {
-		fmt.Printf("parse signed failed: %s", err)
-	}
-
-	// Now we can verify the signature on the payload. An error here would
-	// indicate the the message failed to verify, e.g. because the signature was
-	// broken or the message was tampered with.
-	priv, ok := Priv.Key.(*rsa.PrivateKey)
-	if !ok {
-		fmt.Println("to rsa private key failed")
-	}
-	output, err := object.Verify(priv.Public())
-	if err != nil {
-		fmt.Printf("Verify failed: %s", err)
-	}
-
-	fmt.Printf(string(output))
-	fmt.Println("=========veriry========\n\n")
-
-	fmt.Println("=========oidc verify========\n\n")
-	ctx := context.Background()
-	provider, err := oidc.NewProvider(ctx, "https://dex.example.com:8080")
-	if err != nil {
-		fmt.Println("new provider failed: ", err)
-	}
-	oidcConfig := &oidc.Config{
-		ClientID: "example-app",
-	}
-	verifier := provider.Verifier(oidcConfig)
-	token, err := verifier.Verify(ctx, idToken)
-	if err != nil {
-		fmt.Println("valify failed: ", err)
-	}
-	fmt.Println("token: ", token)
-	fmt.Println("=========oidc verify========\n\n")
-
 	response.WriteEntity(&idToken)
-}
-
-func newUUID() string {
-	u := make([]byte, 16)
-	if _, err := io.ReadFull(rand.Reader, u); err != nil {
-		panic(err)
-	}
-
-	u[8] = (u[8] | 0x80) & 0xBF
-	u[6] = (u[6] | 0x40) & 0x4F
-
-	return hex.EncodeToString(u)
-}
-
-//CreateKeyPair is
-func CreateKeyPair() (pub, priv jose.JSONWebKey) {
-	key, err := rsa.GenerateKey(rand.Reader, 2048)
-	if err != nil {
-		log.Fatalf("gen rsa key: %v", err)
-	}
-	priv = jose.JSONWebKey{
-		Key:       key,
-		KeyID:     "Cgc4OTEyNTU3EgZnaXRodWI",
-		Algorithm: "RS256",
-		Use:       "sig",
-	}
-	pub = jose.JSONWebKey{
-		Key:       key.Public(),
-		KeyID:     "Cgc4OTEyNTU3EgZnaXRodWI",
-		Algorithm: "RS256",
-		Use:       "sig",
-	}
-
-	Ver = jose.JSONWebKey{
-		Key:       key.Public(),
-		KeyID:     "Cgc4OTEyNTU3EgZnaXRodWI",
-		Algorithm: "RS256",
-		Use:       "sig",
-	}
-
-	return pub, priv
 }
 
 func handlePublicKeys(request *restful.Request, response *restful.Response) {
@@ -322,56 +122,11 @@ func handlePublicKeys(request *restful.Request, response *restful.Response) {
 		Keys: make([]jose.JSONWebKey, 1),
 	}
 	jwks.Keys[0] = Pub
-	//TODO VerificationKeys
 
 	fmt.Printf("public keys: %v", jwks)
 
 	response.AddHeader("Content-Type", "application/json")
 	response.WriteEntity(&jwks)
-}
-
-// GET http://localhost:8080/users/1
-//
-func (u UserResource) findUser(request *restful.Request, response *restful.Response) {
-	id := request.PathParameter("user-id")
-	response.WriteEntity(&User{id, "fanux"})
-}
-
-// PUT http://localhost:8080/users/1
-// <User><ID>1</ID><Name>Melissa</Name></User>
-//
-func (u *UserResource) updateUser(request *restful.Request, response *restful.Response) {
-	usr := new(User)
-	err := request.ReadEntity(&usr)
-	if err == nil {
-		u.users[usr.ID] = *usr
-		response.WriteEntity(usr)
-	} else {
-		response.AddHeader("Content-Type", "text/plain")
-		response.WriteErrorString(http.StatusInternalServerError, err.Error())
-	}
-}
-
-// POST http://localhost:8080/users
-// <User><ID>1</ID><Name>Melissa Raspberry</Name></User>
-//
-func (u *UserResource) createUser(request *restful.Request, response *restful.Response) {
-	usr := User{ID: request.PathParameter("user-id")}
-	err := request.ReadEntity(&usr)
-	if err == nil {
-		u.users[usr.ID] = usr
-		response.WriteHeaderAndEntity(http.StatusCreated, usr)
-	} else {
-		response.AddHeader("Content-Type", "text/plain")
-		response.WriteErrorString(http.StatusInternalServerError, err.Error())
-	}
-}
-
-// DELETE http://localhost:8080/users/1
-//
-func (u *UserResource) removeUser(request *restful.Request, response *restful.Response) {
-	id := request.PathParameter("user-id")
-	delete(u.users, id)
 }
 
 func main() {
