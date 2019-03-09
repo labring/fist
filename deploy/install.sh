ffb10681c7e4dd696b8dd8b0522f19598514a540
fanux <fhtjob@hotmail.com>
2019-03-09 18:17:47 +0800

    fix shell

--- deploy/install.sh
+++ deploy/install.sh
@@ -1,3 +1,3 @@
-cd auth/deploy && sh install.sh
+cd ../auth/deploy && sh install.sh
 echo "10.106.233.67 fist.sealyun.svc.cluster.local" >> /etc/hosts
-cd - && cd terminal/deploy && kubectl create -f rbac.yaml && kubectl create -f deploy.yaml
+cd - && cd ../terminal/deploy && kubectl create -f rbac.yaml && kubectl create -f deploy.yaml
