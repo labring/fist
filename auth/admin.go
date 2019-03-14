1e760a98745a5b661a8aae31c32270531029bc8f
fanux <474785153@qq.com>
2019-03-14 21:18:58 +0800

    reset to before lint

--- auth/admin.go
+++ auth/admin.go
@@ -5,8 +5,10 @@ import (
 	"github.com/fanux/fist/tools"
 )
 
-// vars
+//AdminUsername is global vars for admin username
 var AdminUsername string
+
+//AdminPassword is global vars for admin password
 var AdminPassword string
 
 // consts
@@ -15,22 +17,26 @@ const (
 	DefaultSecretName = "fist-admin"
 )
 
+// Admin is struct of Adminer interface
 type Admin struct {
 	Name   string
 	Passwd string
 }
 
+// Adminer is admin.go interface
 type Adminer interface {
 	LoadSecret() error
 	IsAdmin() (bool, error)
 }
 
+// NewAdmin is Admin struct init function
 func NewAdmin(name string, passwd string) Adminer {
 	var admire Adminer
 	admire = &Admin{Name: name, Passwd: passwd}
 	return admire
 }
 
+// LoadSecret is implements for Adminer  function
 func (*Admin) LoadSecret() error {
 	clients, err := tools.GetK8sClient()
 	if err != nil {
@@ -53,6 +59,7 @@ func (*Admin) LoadSecret() error {
 	return nil
 }
 
+// IsAdmin is implements for Adminer  function
 func (admin *Admin) IsAdmin() (bool, error) {
 	if admin.Name == "" {
 		return false, errors.New("the username is empty")
@@ -60,9 +67,8 @@ func (admin *Admin) IsAdmin() (bool, error) {
 	if admin.Passwd == "" {
 		return false, errors.New("the password is empty")
 	}
-	if admin.Name == AdminUsername && admin.Passwd == AdminPassword {
-		return true, nil
-	} else {
+	if admin.Name != AdminUsername || admin.Passwd != AdminPassword {
 		return false, errors.New("the username and password is mismatching")
 	}
+	return true, nil
 }
