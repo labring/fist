1e9d2f075ba88e980848a7964838b0e688035cbd
cuisongliu <cuisongliu@qq.com>
2019-03-14 14:29:58 +0800

    Revert "新增drone配置并修复lint代码时的错误"

--- auth/admin.go
+++ auth/admin.go
@@ -5,10 +5,8 @@ import (
 	"github.com/fanux/fist/tools"
 )
 
-//AdminUsername is global vars for admin username
+// vars
 var AdminUsername string
-
-//AdminPassword is global vars for admin password
 var AdminPassword string
 
 // consts
@@ -17,26 +15,22 @@ const (
 	DefaultSecretName = "fist-admin"
 )
 
-// Admin is struct of Adminer interface
 type Admin struct {
 	Name   string
 	Passwd string
 }
 
-// Adminer is admin.go interface
 type Adminer interface {
 	LoadSecret() error
 	IsAdmin() (bool, error)
 }
 
-// NewAdmin is Admin struct init function
 func NewAdmin(name string, passwd string) Adminer {
 	var admire Adminer
 	admire = &Admin{Name: name, Passwd: passwd}
 	return admire
 }
 
-// LoadSecret is implements for Adminer  function
 func (*Admin) LoadSecret() error {
 	clients, err := tools.GetK8sClient()
 	if err != nil {
@@ -59,7 +53,6 @@ func (*Admin) LoadSecret() error {
 	return nil
 }
 
-// IsAdmin is implements for Adminer  function
 func (admin *Admin) IsAdmin() (bool, error) {
 	if admin.Name == "" {
 		return false, errors.New("the username is empty")
@@ -67,8 +60,9 @@ func (admin *Admin) IsAdmin() (bool, error) {
 	if admin.Passwd == "" {
 		return false, errors.New("the password is empty")
 	}
-	if admin.Name != AdminUsername || admin.Passwd != AdminPassword {
+	if admin.Name == AdminUsername && admin.Passwd == AdminPassword {
+		return true, nil
+	} else {
 		return false, errors.New("the username and password is mismatching")
 	}
-	return true, nil
 }
--- tools/response.go
+++ tools/response.go
@@ -8,12 +8,12 @@ type responseObject struct {
 	Data    interface{} `json:"data"`
 }
 
-//ResponseSuccess is web response for success
+//ResponseSuccess
 func ResponseSuccess(response *restful.Response, data interface{}) {
 	response.WriteEntity(responseObject{Code: 200, Message: "success", Data: data})
 }
 
-//ResponseError is web response for success
+//ResponseError
 func ResponseError(response *restful.Response, err error) {
 	response.WriteEntity(responseObject{Code: 500, Message: err.Error(), Data: ""})
 }
