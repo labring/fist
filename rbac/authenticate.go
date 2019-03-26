7c389d9ab0ec8dccbad59f7a4cc6529882873096
杜嘉成 <dujiacheng@zccp.com>
2019-03-26 10:56:18 +0800

    Authenticate by ldap

--- rbac/authenticate.go
+++ rbac/authenticate.go
@@ -1,5 +1,8 @@
 package rbac
 
+import (
+	"strings"
+)
 //DoAuthentication is user login access function
 func DoAuthentication(user, password string) *UserInfo {
 	var authenticators = []authenticator{newAdminAuth(), newLdapAuth(), newKubeSecretAuth()}
@@ -75,6 +78,38 @@ func (KubeSecretAuth) Authenticate(user, password string) *UserInfo {
 func (LdapAuth) Authenticate(user, password string) *UserInfo {
 	if RbacLdapEnable {
 		//if user enable ldap
+        l, err := ldap.Dial("tcp", fmt.Sprintf("%s:%d", RbacLdapHost, RbacLdapPort))
+        if err != nil {
+                log.Fatal(err)
+        }
+        defer l.Close()
+
+        err = l.Bind(RbacLdapBindDN, RbacLdapBindPassword)
+        if err != nil {
+                log.Fatal(err)
+        }
+        ldapdn :=  strings.Split(RbacLdapBindDN, ",")    
+        dc := ldapdn[1] + string(',') +  ldapdn[2] // dc=sealyun,dc=com
+        searchRequest := ldap.NewSearchRequest(
+                dc, ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
+                fmt.Sprintf("(&(objectClass=organizationalPerson)(uid=%s))", user),
+                []string{ "dn"},
+                nil,
+        )
+        sr, err := l.Search(searchRequest)
+        if err != nil {
+                log.Fatal(err)
+        }
+
+        userdn := sr.Entries[0].DN
+        // Bind as the user to verify their password
+        err = l.Bind(userdn, password)
+        if err != nil {
+                log.Fatal(err)
+        }
+        log.Fatal("user authenticated")
+
+		return &UserInfo{Username: user , Nickname: sr.Entries[0].GetAttributeValue("cn"), Password: password}
 	}
 	return nil
 }
