cfe6db60eb974c713b225fd7eddfea2eb55bb223
cuisongliu <cuisongliu@qq.com>
2019-04-03 00:03:24 +0800

    [SKIP CI]fix auth http port

--- auth/server_k8s.go
+++ auth/server_k8s.go
@@ -42,7 +42,7 @@ func discoveryHandler(request *restful.Request, response *restful.Response) {
 	dis := &discovery{
 		Issuer:      "https://fist.sealyun.svc.cluster.local" + authHTTPSPortString,
 		Auth:        "https://fist.sealyun.svc.cluster.local" + authHTTPSPortString + "/auth",
-		Token:       "https://fist.sealyun.svc.cluster.local" + authHTTPSPortString + "/token",
+		Token:       "http://fist.sealyun.svc.cluster.local" + authHTTPPortString + "/token",
 		Keys:        "https://fist.sealyun.svc.cluster.local" + authHTTPSPortString + "/keys",
 		Subjects:    []string{"public"},
 		IDTokenAlgs: []string{string(jose.RS256)},
