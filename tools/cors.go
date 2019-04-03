668b968855fb9881d58d407c085f62f1ddef4ba2
cuisongliu <cuisongliu@qq.com>
2019-04-03 14:45:22 +0800

    fix cors

--- tools/cors.go
+++ tools/cors.go
@@ -9,7 +9,6 @@ func Cors(wsContainer *restful.Container) {
 		ExposeHeaders:  []string{"X-My-Header"},
 		AllowedHeaders: []string{"Content-Type", "Accept"},
 		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE"},
-		AllowedDomains: []string{"*"},
 		CookiesAllowed: true,
 		Container:      wsContainer}
 	wsContainer.Filter(cors.Filter)
